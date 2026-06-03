35416e74a8bc3c179bcca67570fa1c2359a1691b
N0mena <alida.rajaonarivelo@gmail.com>
2026-06-03 09:32:31 +0300

    feat: first and second exo employeeList function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { Admin } from "react-admin";
-import { Layout } from "./Layout";
+import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import { EmployeeList } from "./employees/employeeList";
 
 const dataProvider = jsonServerProvider("http://localhost:3002");
 
-export const App = () => <Admin dataProvider={dataProvider}></Admin>;
+export const App = () => <Admin dataProvider={dataProvider}>
+    <Resource name="employees"  list={EmployeeList}/>
+</Admin>;
---
+++ src/employees/employeeList.tsx
@@ -0,0 +1,50 @@
+import {
+    List,
+    Datagrid,
+    TextField,
+    NumberField,
+    BooleanField,
+    EditButton,
+    DeleteButton,
+    SearchInput,
+    SelectInput
+} from "react-admin";
+
+const employeeFilters = [
+    <SearchInput source="q" alwaysOn />,
+    <SelectInput
+        source="department"
+        label="Département"
+        choices={[
+            { id: "Informatique", name: "Informatique" },
+            { id: "Marketing", name: "Marketing" },
+            { id: "RH", name: "RH" },
+            { id: "Finance", name: "Finance" }
+        ]}
+    />
+];
+
+export const EmployeeList = () => (
+    <List filters={employeeFilters} perPage={5}>
+        <Datagrid rowClick="edit">
+            <TextField source="firstname" label="Prénom" />
+            <TextField source="lastname" label="Nom" />
+            <TextField source="email" label="Email" />
+            <TextField source="department" label="Département" />
+
+            <NumberField
+                source="salary"
+                label="Salaire"
+                options={{
+                    style: "currency",
+                    currency: "Ar"
+                }}
+            />
+
+            <BooleanField source="active" label="Actif" />
+
+            <EditButton />
+            <DeleteButton />
+        </Datagrid>
+    </List>
+);
