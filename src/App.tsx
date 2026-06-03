67909283eeefbe0ec682f3853bd9b43c662027a7
N0mena <alida.rajaonarivelo@gmail.com>
2026-06-03 09:57:55 +0300

    feat: employeeShow function

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { EmployeeList } from "./employees/employeeList";
 import { EmployeeCreate } from "./employees/employeeCreate";
+import { EmployeeShow } from "./employees/employeeShow";
 
 const dataProvider = jsonServerProvider("http://localhost:3002");
 
@@ -9,5 +10,6 @@ export const App = () => <Admin dataProvider={dataProvider}>
     <Resource name="employees"  
     list={EmployeeList}
     create={EmployeeCreate}
-    edit={EmployeeCreate}/>
+    edit={EmployeeCreate}
+    show={EmployeeShow}/>
 </Admin>;
